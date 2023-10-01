807e937e29d4e233cc9afa17644b31b765cf462f
vlad <plisakvlad1@gmail.com>
2023-10-01 13:36:42 +0300

    class

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("help me")
+class Student():
+    def _init_(self,name,surname,age,logiks,group,login,password,telegram):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.logiks = logiks
+        self.group = group
+        self.login = login
+        self.password = password
+        self.telegram = telegram
+
+    def all_info(self):
+        print("All infomation of student")
+        print("Name -",self.name)
+        print("Surname -",self.name)
+        print("Age -",self.name)
+        print("Logiks -",self.name)
+        print("Group -",self.name)
+        print("Login -",self.name)
+        print("Password -",self.name)
+        print("Telegram -",self.name)
+        print("===========================================================")
+    def logik_info(self):
+        print("=========================Logiks of Student=========================")
+        lg = ""
+        for logik in self.logiks:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
